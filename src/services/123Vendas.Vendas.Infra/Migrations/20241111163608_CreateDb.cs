d3909c8e4ecff4cbd82d02618e8cfd723f490447
alvarokramer <alvarokramer@gmail.com>
2024-11-11 14:27:17 -0300

    feat: setup to banco de dados (Sqlite)

---
+++ src/services/123Vendas.Vendas.Infra/Migrations/20241111163608_CreateDb.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _123Vendas.Vendas.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Compras",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    NumeroCompra = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataCompra = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClienteId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Desconto = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Filial = table.Column<string>(type: "TEXT", nullable: false),
+                    CompraStatus = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompraItens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CompraId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ProdutoId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ProdutoNome = table.Column<string>(type: "TEXT", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false),
+                    ValorUnitario = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompraItens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompraItens_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraItens_CompraId",
+                table: "CompraItens",
+                column: "CompraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompraItens");
+
+            migrationBuilder.DropTable(
+                name: "Compras");
+        }
+    }
+}
